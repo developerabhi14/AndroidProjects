9c5e3546263b4412379c8a1a0a169db4bb9ab93f
Abhishek <readeravskh@gmail.com>
2025-11-24 07:00:26 +0545

    Menu added in the project

--- Project6SharedPreferencesDatabase/app/src/main/java/com/developerabhi/sharedpreferencesdatabase/ActivityHome.java
+++ Project6SharedPreferencesDatabase/app/src/main/java/com/developerabhi/sharedpreferencesdatabase/ActivityHome.java
@@ -1,18 +1,24 @@
 package com.developerabhi.sharedpreferencesdatabase;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,6 +29,7 @@ public class ActivityHome extends AppCompatActivity {
     TextView totalStudents;
     Button addData;
     ListView lv;
+    Toolbar tb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,12 @@ public class ActivityHome extends AppCompatActivity {
         totalStudents = findViewById(R.id.totalStudents);
         addData = findViewById(R.id.addData);
         lv = findViewById(R.id.lv);
+
+        tb = findViewById(R.id.toolbar);
+
+        setSupportActionBar(tb);
+
+        getSupportActionBar().setTitle("abc");
 
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,12 +69,12 @@ public class ActivityHome extends AppCompatActivity {
                         String address1 = address.getText().toString();
                         String faculty1 = faculty.getText().toString();
 
-                        DataModel dm=new DataModel();
+                        DataModel dm = new DataModel();
                         dm.setName(name1);
                         dm.setAddress(address1);
                         dm.setFaculty(faculty1);
 
-                        DatabaseHelper db=new DatabaseHelper(ActivityHome.this);
+                        DatabaseHelper db = new DatabaseHelper(ActivityHome.this);
                         db.insertData(dm);
                         dialog.dismiss();
                         onStart();
@@ -80,11 +93,35 @@ public class ActivityHome extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        ArrayList<DataModel> dm=new ArrayList<>();
-        DatabaseHelper db=new DatabaseHelper(ActivityHome.this);
-        dm=db.getData();
-        int count=dm.size();
-        totalStudents.setText("Total Students = "+count);
-        lv.setAdapter(new CustomAdapter(ActivityHome.this,dm));
+        ArrayList<DataModel> dm = new ArrayList<>();
+        DatabaseHelper db = new DatabaseHelper(ActivityHome.this);
+        dm = db.getData();
+        int count = dm.size();
+        totalStudents.setText("Total Students = " + count);
+        lv.setAdapter(new CustomAdapter(ActivityHome.this, dm));
+    }
+
+    //menu layout inflate
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menufile, menu);
+        return true;
+    }
+
+    //setting clicks and writing lgout logic
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            SharedPreferences sp = getSharedPreferences("loginStatus", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("status", false);
+            editor.commit();
+            Toast.makeText(ActivityHome.this, "Logout Successfull", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(ActivityHome.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+        return true;
     }
 }
